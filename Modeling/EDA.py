b265ab06f8089fca0a560ad6d62feddf38acd587
Rajan <rajandes7@gmail.com>
2021-04-29 19:13:32 -0500

    Finished Random Forest and LDA along with more plots.

--- Modeling/EDA.py
+++ Modeling/EDA.py
@@ -1,27 +1,59 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 import seaborn as sns
 import missingno as msno
-
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 df = pd.read_csv('data/cleandata/MergedComplete.csv')
-df_labels = df.columns.to_list()
 
-def draw_histograms(df, variables, n_rows, n_cols, bin_size=100):
-    """Draws a grid of 100 bin histograms with n_rows x n_cols"""
+df[['State', 'Power Source']] = df[['State', 'Power Source']].astype('category')
+df_learn = df.drop(['Power Source','State'] ,axis=1)
+
+X_train, X_test, y_train, y_test = \
+    train_test_split(df_learn, df['Power Source'],
+                     test_size=0.3,
+                     random_state=42)
+
+feat_names = X_train.columns.to_list()
+X_train.reset_index(drop=True, inplace=True)
+
+def draw_scatters(df, y, variables, n_rows, n_cols,
+                    filename='plot'):
+    """Plots each column of pandas dataframe against supervisor on
+    a grid specified by n_rows x n_cols."""
+    fig=plt.figure()
+    for i, var_name in enumerate(variables):
+        ax=fig.add_subplot(n_rows,n_cols,i+1)
+        plt.scatter(df[var_name],y)
+        ax.set_title(var_name)
+    fig.tight_layout()  # Improves appearance a bit.
+    plt.savefig(filename + '.png')
+
+
+draw_scatters(X_train,y_train,feat_names,3,3,
+              filename='data/Plots/Scatter_Plots')
+
+
+def draw_histograms(df, variables, n_rows, n_cols, bin_size=100,
+                    filename='plot'):
+    """Draws a grid of 100 bin histograms with n_rows x n_cols
+    for each column of a pandas dataframe"""
     fig=plt.figure()
     for i, var_name in enumerate(variables):
         ax=fig.add_subplot(n_rows,n_cols,i+1)
         df[var_name].hist(bins=bin_size,ax=ax)
         ax.set_title(var_name+" Distribution")
     fig.tight_layout()  # Improves appearance a bit.
-    plt.show()
+    plt.savefig(filename + '.png')
 
-draw_histograms(df,['Power Source','Longitude', 'Latitude',
-                    'Capacity','GHI','DNI','a30','a35','a40'],3,3,bin_size=50)
-plt.title('Feature Histograms')
-plt.savefig('data/Plots/histograms.png')
+
+draw_histograms(X_train,feat_names,3,3,bin_size=50,
+                filename='data/Plots/Feature_Hists')
+
 
 def sborn_heatmap(df,filename='plot',title='Plot'):
     """Plots and saves a seaborn heatmap to filename as a png"""
@@ -29,19 +61,40 @@ def sborn_heatmap(df,filename='plot',title='Plot'):
     mask = np.triu(np.ones_like(corr, dtype=bool))
     plt.subplots(figsize=(11, 9))
     cmap = sns.diverging_palette(230, 20, as_cmap=True)
-    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,
+    sns.heatmap(corr, mask=mask, cmap=cmap, center=0,annot=True,
                 square=True, linewidths=.5, cbar_kws={"shrink": .5})
     plt.title(title)
     plt.savefig(filename + '.png')
-sborn_heatmap(df,filename='data/Plots/correlation',
-              title='Correlation Plot')
 
-def missing_matrix(df, filename='plot', title='Plot'):
+
+sborn_heatmap(X_train,filename='data/Plots/Corr_Heatmap',
+              title='Correlation Heatmap (Training Data)')
+
+
+def missing_matrix(df, filename='plot'):
     """Creates missingness plot and saves it as png at filename"""
     msno.matrix(df)
-    plt.title(title)
     plt.savefig(filename + '.png')
-missing_matrix(df,filename='data/Plots/missingdata',
-               title='Missing Values')
 
-# TODO Screen for outliers
+missing_matrix(X_train,filename='data/Plots/Missing_Data')
+
+
+def impute_scale_pca(df, components, filename='plot'):
+    """Returns sklearn.PCA object for a dataset after imputing/scaling. Saves
+    the plot of the first two PC scores at filename."""
+    ImputeX_train = KNNImputer(add_indicator=False).fit_transform(df)
+    Scale_X = StandardScaler().fit_transform(ImputeX_train)
+    pca = PCA(n_components=components)
+    PC_scores = pca.fit_transform(Scale_X)
+    plt.figure()
+    plt.scatter(PC_scores[:,0],PC_scores[:,1])
+    plt.title('PC1 vs PC2')
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.savefig(filename + '.png')
+    return pca
+
+
+impute_scale_pca(X_train,components = 2,
+                 filename='data/Plots/PC_scores')
+
--- Modeling/Models.py
+++ Modeling/Models.py
@@ -1,28 +1,34 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.impute import KNNImputer
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, plot_roc_curve
 from sklearn.linear_model import LogisticRegressionCV
-from sklearn.model_selection import GridSearchCV
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+import gradio as gr
 
 df = pd.read_csv('data/cleandata/MergedComplete.csv')
 
 df[['State', 'Power Source']] = df[['State', 'Power Source']].astype('category')
 df_learn = df.drop(['Power Source', 'State'] ,axis=1)
+feat_names = df_learn.columns
 
 X_train, X_test, y_train, y_test = \
     train_test_split(df_learn, df['Power Source'],
                      test_size=0.3,
                      random_state=42)
 
-
 knn_impute = KNNImputer()
 scaler = StandardScaler()
 
-# TODO: Penalized LDA, Trees/Bagging, Boosting, Plots (ROC curves, variable importance)
+# TODO: Trees/Bagging
+# TODO: Plots (ROC curves, variable importance), check for well calibrated probabilities
 
 # Logistic Elastic Net
 # log_elastic_net = LogisticRegressionCV(cv=5,
@@ -30,13 +36,18 @@ scaler = StandardScaler()
 #                                        solver='saga',
 #                                        random_state=42,
 #                                        max_iter=10000,
-#                                        l1_ratios=[0.0001,0.1,0.12,0.14,0.16,0.18,0.2] )
+#                                        l1_ratios=[0,0.2,0.4,0.6,0.8,.9,1] )
 #
 # pipe = Pipeline( [  ('impute',  knn_impute),
 #                     ('scale',   scaler),
 #                     ('elasticnet', log_elastic_net ) ])
 #
 # pipe.fit(X_train,y_train)
+# coefs = pd.DataFrame(pipe[2].coef_, columns=df_learn.columns)
+# plt.bar(x=feat_names, height=abs(coefs.values.flatten())/(abs(coefs.values).sum()))
+# plt.title('Logistic Elastic Net Feature Importance: L1_ratio = 0, Lambda = 0.0464')
+# plt.xticks(rotation=45)
+# plt.savefig('data/Plots/Logistic_Feat_Imp.png')
 #
 # print('Tuned Cost Parameter: ' + str(pipe[2].C_))
 # print('Tuned L1 ratio: ' + str(pipe[2].l1_ratio_))
@@ -49,12 +60,14 @@ scaler = StandardScaler()
 # y_pred = pipe.predict(X_test)
 # print('Test Accuracy: ' + str(pipe.score(X_test,y_test)))
 # print(classification_report(y_test, y_pred))
+#
+# plot_roc_curve(pipe,X_test,y_test)
+# plt.title('Logistic Elastic Net: L1_Ratio = 0, Lambda = 0.0464')
+# plt.savefig('data/Plots/Logistic_ROC_Curve.png')
 
-
-
-# Support Vector Classifier/Machine
-# param_grid = {'C': [0.1, 1, 10, 100, 1000],
-#               'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
+# Support Vector Machine
+# param_grid = {'C': [0.1, 1, 10, 100, 500, 1000],
+#               'gamma': [0.18, 0.27, 0.67],
 #               'kernel': ['linear','rbf']}
 #
 # svc = GridSearchCV(SVC(random_state=42), param_grid, refit = True, verbose = 0)
@@ -71,8 +84,83 @@ scaler = StandardScaler()
 # y_pred = pipe.predict(X_test)
 # print('Test Accuracy: ' + str(pipe.score(X_test,y_test)))
 # print(classification_report(y_test, y_pred))
-
+#
+# plot_roc_curve(pipe,X_test,y_test)
+# plt.title('SVM w/ RBF Kernel, C = 10, Gamma = 0.67')
+# plt.savefig('data/Plots/SVM_ROC_Curve.png')
 
 # Penalized LDA
+# lda = LinearDiscriminantAnalysis(solver='eigen',shrinkage='auto')
+# pipe = Pipeline( [  ('impute',  knn_impute),
+#                     ('scale',   scaler),
+#                     ('LDA',     lda ) ])
+#
+#
+# pipe.fit(X_train,y_train)
+# print('Training Accuracy: ' + str(pipe.score(X_train,y_train)))
+#
+# coefs = pd.DataFrame(pipe[2].coef_, columns=df_learn.columns)
+# plt.bar(x=feat_names, height=abs(coefs.values.flatten())/(abs(coefs.values).sum()))
+# plt.title('LDA Feature Importance')
+# plt.xticks(rotation=45)
+# plt.savefig('data/Plots/LDA_Feat_Imp.png')
+#
+# y_pred = pipe.predict(X_test)
+# print('Test Accuracy: ' + str(pipe.score(X_test,y_test)))
+# print(classification_report(y_test, y_pred))
+#
+# plot_roc_curve(pipe,X_test,y_test)
+# plt.title('LDA ')
+# plt.savefig('data/Plots/LDA_ROC_Curve.png')
 
-# Trees/Bagging/Boosting
+# Random Forest
+rf = RandomForestClassifier(n_estimators=500, criterion='gini',
+                            max_features='sqrt', oob_score=True,
+                            random_state=42)
+
+pipe = Pipeline( [  ('impute',  knn_impute),
+                    ('scale',   scaler),
+                    ('rf',     rf ) ])
+
+
+pipe.fit(X_train,y_train)
+print('Training Accuracy: ' + str(pipe.score(X_train,y_train)))
+print('OOB Error: ' + str(pipe[2].oob_score_))
+
+# feat_imp = pipe[2].feature_importances_
+# plt.bar(feat_names,feat_imp)
+# plt.xticks(rotation=45)
+# plt.title('Random Forest Classifier: Feature Importance (B = 500)')
+# plt.savefig('data/Plots/RF_Feat_Imp.png')
+
+y_pred = pipe.predict(X_test)
+print('Test Accuracy: ' + str(pipe.score(X_test,y_test)))
+print(classification_report(y_test, y_pred))
+
+# plot_roc_curve(pipe,X_test,y_test)
+# plt.title('Random Forest Classifier (B = 500)')
+# plt.savefig('data/Plots/RF_ROC_Curve.png')
+
+
+def Farm_Predictor(DNI, GHI, a40, a35, a30, Capacity, Longitude, Latitude):
+    df = pd.DataFrame.from_dict({'DNI':[DNI], 'GHI':[GHI], 'a40':[a40],
+                                 'a35':[a35], 'a30':[a30], 'Capacity':[Capacity],
+                                 'Longitude':[Longitude], 'Latitude': [Latitude]})
+    pred = pipe.predict_proba(df)[0]
+    return {'Solar': pred[0], 'Wind': pred[1]}
+
+Source = gr.inputs.Radio(['Solar','Wind'], label='Power Source')
+DNI = gr.inputs.Slider(minimum=0, maximum=10, default=4, label="DNI")
+GHI = gr.inputs.Slider(minimum=0, maximum=10, default=4, label="GHI")
+a40 = gr.inputs.Slider(minimum=0, maximum=500, default=100, label="a40 Wind Potential (MW)")
+a35 = gr.inputs.Slider(minimum=0, maximum=500, default=100, label="a35 Wind Potential (MW)")
+a30 = gr.inputs.Slider(minimum=0, maximum=500, default=100, label="a30 Wind Potential (MW)")
+Capacity = gr.inputs.Slider(minimum=0, maximum=2500, default=100, label="Capacity (MW)")
+Longitude = gr.inputs.Slider(minimum=-172, maximum=145, default=100, label="Longitude")
+Latitude = gr.inputs.Slider(minimum=13, maximum=50, default=20, label="Latitude")
+
+
+gr.Interface(Farm_Predictor(), [DNI, GHI, a40, a35, a30, Capacity, Longitude, Latitude],
+             "label", live=True).launch()
+
+
